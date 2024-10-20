38615212d85c1dd0a33f9303b5748be6b5de0118
Jonnier <alzatejonny39@gmail.com>
2024-10-20 10:06:39 -0500

    Tienda

---
+++ frmCompra.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace PROCESO_CRUD
+{
+    partial class fmrCompra
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbCompra = new System.Windows.Forms.GroupBox();
+            this.pbProducto = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gbCompra.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbProducto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbCompra
+            // 
+            this.gbCompra.Controls.Add(this.label3);
+            this.gbCompra.Controls.Add(this.label2);
+            this.gbCompra.Controls.Add(this.linkLabel1);
+            this.gbCompra.Controls.Add(this.label1);
+            this.gbCompra.Controls.Add(this.pbProducto);
+            this.gbCompra.Location = new System.Drawing.Point(12, 3);
+            this.gbCompra.Name = "gbCompra";
+            this.gbCompra.Size = new System.Drawing.Size(244, 410);
+            this.gbCompra.TabIndex = 1;
+            this.gbCompra.TabStop = false;
+            this.gbCompra.Text = "Producto Elegido";
+            // 
+            // pbProducto
+            // 
+            this.pbProducto.Location = new System.Drawing.Point(17, 34);
+            this.pbProducto.Name = "pbProducto";
+            this.pbProducto.Size = new System.Drawing.Size(198, 147);
+            this.pbProducto.TabIndex = 0;
+            this.pbProducto.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(17, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 45);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "NOTA: para continuar con la compra comunicate con nosotros en nuestras distintas " +
+    "redes sociales";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkVisited = true;
+            this.linkLabel1.Location = new System.Drawing.Point(17, 255);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(191, 13);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://www.instagram.com/jonni.g21/";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 294);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "WhatsApp  3024158318";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 328);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Instagram  OrigenMedellin";
+            // 
+            // fmrCompra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbCompra);
+            this.Name = "fmrCompra";
+            this.Text = "fmrCompra";
+            this.gbCompra.ResumeLayout(false);
+            this.gbCompra.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbProducto)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbCompra;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pbProducto;
+    }
+}
---
+++ frmGaleria.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace PROCESO_CRUD
+{
+    partial class frmGaleria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbRopa = new System.Windows.Forms.GroupBox();
+            this.btnTenis2 = new System.Windows.Forms.Button();
+            this.btnTenis1 = new System.Windows.Forms.Button();
+            this.btnCamisa2 = new System.Windows.Forms.Button();
+            this.btnCamisa1 = new System.Windows.Forms.Button();
+            this.btnBuso2 = new System.Windows.Forms.Button();
+            this.btnBuso1 = new System.Windows.Forms.Button();
+            this.lblPrecioTenis2 = new System.Windows.Forms.Label();
+            this.lblTenis2 = new System.Windows.Forms.Label();
+            this.lblPrecioTenis1 = new System.Windows.Forms.Label();
+            this.lblTenis1 = new System.Windows.Forms.Label();
+            this.lblPrecioCamisa2 = new System.Windows.Forms.Label();
+            this.lblcamisa2 = new System.Windows.Forms.Label();
+            this.lblPrecioCamisa1 = new System.Windows.Forms.Label();
+            this.lblcamisa1 = new System.Windows.Forms.Label();
+            this.lblPrecioAcab2 = new System.Windows.Forms.Label();
+            this.lblAcab2 = new System.Windows.Forms.Label();
+            this.lblPrecioAcab1 = new System.Windows.Forms.Label();
+            this.lblAcab1 = new System.Windows.Forms.Label();
+            this.pbTenis1 = new System.Windows.Forms.PictureBox();
+            this.pbTenis2 = new System.Windows.Forms.PictureBox();
+            this.pbCamisa2 = new System.Windows.Forms.PictureBox();
+            this.pbCamisa1 = new System.Windows.Forms.PictureBox();
+            this.pbBuso1 = new System.Windows.Forms.PictureBox();
+            this.pbBuso2 = new System.Windows.Forms.PictureBox();
+            this.gbRopa.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTenis1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTenis2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCamisa2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCamisa1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBuso1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBuso2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbRopa
+            // 
+            this.gbRopa.Controls.Add(this.btnTenis2);
+            this.gbRopa.Controls.Add(this.btnTenis1);
+            this.gbRopa.Controls.Add(this.btnCamisa2);
+            this.gbRopa.Controls.Add(this.btnCamisa1);
+            this.gbRopa.Controls.Add(this.btnBuso2);
+            this.gbRopa.Controls.Add(this.btnBuso1);
+            this.gbRopa.Controls.Add(this.lblPrecioTenis2);
+            this.gbRopa.Controls.Add(this.lblTenis2);
+            this.gbRopa.Controls.Add(this.lblPrecioTenis1);
+            this.gbRopa.Controls.Add(this.lblTenis1);
+            this.gbRopa.Controls.Add(this.lblPrecioCamisa2);
+            this.gbRopa.Controls.Add(this.lblcamisa2);
+            this.gbRopa.Controls.Add(this.lblPrecioCamisa1);
+            this.gbRopa.Controls.Add(this.lblcamisa1);
+            this.gbRopa.Controls.Add(this.lblPrecioAcab2);
+            this.gbRopa.Controls.Add(this.lblAcab2);
+            this.gbRopa.Controls.Add(this.lblPrecioAcab1);
+            this.gbRopa.Controls.Add(this.lblAcab1);
+            this.gbRopa.Controls.Add(this.pbTenis1);
+            this.gbRopa.Controls.Add(this.pbTenis2);
+            this.gbRopa.Controls.Add(this.pbCamisa2);
+            this.gbRopa.Controls.Add(this.pbCamisa1);
+            this.gbRopa.Controls.Add(this.pbBuso1);
+            this.gbRopa.Controls.Add(this.pbBuso2);
+            this.gbRopa.Location = new System.Drawing.Point(13, 12);
+            this.gbRopa.Name = "gbRopa";
+            this.gbRopa.Size = new System.Drawing.Size(561, 459);
+            this.gbRopa.TabIndex = 4;
+            this.gbRopa.TabStop = false;
+            this.gbRopa.Text = "CATALOGO";
+            // 
+            // btnTenis2
+            // 
+            this.btnTenis2.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnTenis2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnTenis2.Location = new System.Drawing.Point(438, 188);
+            this.btnTenis2.Name = "btnTenis2";
+            this.btnTenis2.Size = new System.Drawing.Size(75, 23);
+            this.btnTenis2.TabIndex = 32;
+            this.btnTenis2.Text = "Comprar";
+            this.btnTenis2.UseVisualStyleBackColor = false;
+            // 
+            // btnTenis1
+            // 
+            this.btnTenis1.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnTenis1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnTenis1.Location = new System.Drawing.Point(439, 432);
+            this.btnTenis1.Name = "btnTenis1";
+            this.btnTenis1.Size = new System.Drawing.Size(75, 23);
+            this.btnTenis1.TabIndex = 31;
+            this.btnTenis1.Text = "Comprar";
+            this.btnTenis1.UseVisualStyleBackColor = false;
+            // 
+            // btnCamisa2
+            // 
+            this.btnCamisa2.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnCamisa2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCamisa2.Location = new System.Drawing.Point(246, 432);
+            this.btnCamisa2.Name = "btnCamisa2";
+            this.btnCamisa2.Size = new System.Drawing.Size(75, 23);
+            this.btnCamisa2.TabIndex = 30;
+            this.btnCamisa2.Text = "Comprar";
+            this.btnCamisa2.UseVisualStyleBackColor = false;
+            // 
+            // btnCamisa1
+            // 
+            this.btnCamisa1.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnCamisa1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCamisa1.Location = new System.Drawing.Point(63, 432);
+            this.btnCamisa1.Name = "btnCamisa1";
+            this.btnCamisa1.Size = new System.Drawing.Size(75, 23);
+            this.btnCamisa1.TabIndex = 29;
+            this.btnCamisa1.Text = "Comprar";
+            this.btnCamisa1.UseVisualStyleBackColor = false;
+            // 
+            // btnBuso2
+            // 
+            this.btnBuso2.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnBuso2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnBuso2.Location = new System.Drawing.Point(246, 185);
+            this.btnBuso2.Name = "btnBuso2";
+            this.btnBuso2.Size = new System.Drawing.Size(75, 23);
+            this.btnBuso2.TabIndex = 26;
+            this.btnBuso2.Text = "Comprar";
+            this.btnBuso2.UseVisualStyleBackColor = false;
+            // 
+            // btnBuso1
+            // 
+            this.btnBuso1.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnBuso1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnBuso1.Location = new System.Drawing.Point(63, 185);
+            this.btnBuso1.Name = "btnBuso1";
+            this.btnBuso1.Size = new System.Drawing.Size(75, 23);
+            this.btnBuso1.TabIndex = 25;
+            this.btnBuso1.Text = "Comprar";
+            this.btnBuso1.UseVisualStyleBackColor = false;
+            // 
+            // lblPrecioTenis2
+            // 
+            this.lblPrecioTenis2.AutoSize = true;
+            this.lblPrecioTenis2.Location = new System.Drawing.Point(489, 157);
+            this.lblPrecioTenis2.Name = "lblPrecioTenis2";
+            this.lblPrecioTenis2.Size = new System.Drawing.Size(52, 13);
+            this.lblPrecioTenis2.TabIndex = 24;
+            this.lblPrecioTenis2.Text = "$390.000";
+            // 
+            // lblTenis2
+            // 
+            this.lblTenis2.Location = new System.Drawing.Point(409, 157);
+            this.lblTenis2.Name = "lblTenis2";
+            this.lblTenis2.Size = new System.Drawing.Size(83, 28);
+            this.lblTenis2.TabIndex = 23;
+            this.lblTenis2.Text = "Tenis Creative Talla 9";
+            // 
+            // lblPrecioTenis1
+            // 
+            this.lblPrecioTenis1.AutoSize = true;
+            this.lblPrecioTenis1.Location = new System.Drawing.Point(480, 401);
+            this.lblPrecioTenis1.Name = "lblPrecioTenis1";
+            this.lblPrecioTenis1.Size = new System.Drawing.Size(52, 13);
+            this.lblPrecioTenis1.TabIndex = 22;
+            this.lblPrecioTenis1.Text = "$450.000";
+            // 
+            // lblTenis1
+            // 
+            this.lblTenis1.Location = new System.Drawing.Point(409, 401);
+            this.lblTenis1.Name = "lblTenis1";
+            this.lblTenis1.Size = new System.Drawing.Size(81, 28);
+            this.lblTenis1.TabIndex = 21;
+            this.lblTenis1.Text = "Tenis Creative Talla 8";
+            // 
+            // lblPrecioCamisa2
+            // 
+            this.lblPrecioCamisa2.AutoSize = true;
+            this.lblPrecioCamisa2.Location = new System.Drawing.Point(294, 401);
+            this.lblPrecioCamisa2.Name = "lblPrecioCamisa2";
+            this.lblPrecioCamisa2.Size = new System.Drawing.Size(46, 13);
+            this.lblPrecioCamisa2.TabIndex = 20;
+            this.lblPrecioCamisa2.Text = "$95.000";
+            // 
+            // lblcamisa2
+            // 
+            this.lblcamisa2.Location = new System.Drawing.Point(223, 401);
+            this.lblcamisa2.Name = "lblcamisa2";
+            this.lblcamisa2.Size = new System.Drawing.Size(73, 28);
+            this.lblcamisa2.TabIndex = 19;
+            this.lblcamisa2.Text = "Camisa 420 Talla L";
+            // 
+            // lblPrecioCamisa1
+            // 
+            this.lblPrecioCamisa1.AutoSize = true;
+            this.lblPrecioCamisa1.Location = new System.Drawing.Point(110, 401);
+            this.lblPrecioCamisa1.Name = "lblPrecioCamisa1";
+            this.lblPrecioCamisa1.Size = new System.Drawing.Size(46, 13);
+            this.lblPrecioCamisa1.TabIndex = 18;
+            this.lblPrecioCamisa1.Text = "$95.000";
+            // 
+            // lblcamisa1
+            // 
+            this.lblcamisa1.Location = new System.Drawing.Point(39, 401);
+            this.lblcamisa1.Name = "lblcamisa1";
+            this.lblcamisa1.Size = new System.Drawing.Size(73, 28);
+            this.lblcamisa1.TabIndex = 17;
+            this.lblcamisa1.Text = "Camisa 420 Talla L";
+            // 
+            // lblPrecioAcab2
+            // 
+            this.lblPrecioAcab2.AutoSize = true;
+            this.lblPrecioAcab2.Location = new System.Drawing.Point(294, 154);
+            this.lblPrecioAcab2.Name = "lblPrecioAcab2";
+            this.lblPrecioAcab2.Size = new System.Drawing.Size(52, 13);
+            this.lblPrecioAcab2.TabIndex = 12;
+            this.lblPrecioAcab2.Text = "$200.000";
+            // 
+            // lblAcab2
+            // 
+            this.lblAcab2.Location = new System.Drawing.Point(223, 154);
+            this.lblAcab2.Name = "lblAcab2";
+            this.lblAcab2.Size = new System.Drawing.Size(73, 28);
+            this.lblAcab2.TabIndex = 11;
+            this.lblAcab2.Text = "Buso A.C.A.B Talla L";
+            // 
+            // lblPrecioAcab1
+            // 
+            this.lblPrecioAcab1.AutoSize = true;
+            this.lblPrecioAcab1.Location = new System.Drawing.Point(110, 154);
+            this.lblPrecioAcab1.Name = "lblPrecioAcab1";
+            this.lblPrecioAcab1.Size = new System.Drawing.Size(52, 13);
+            this.lblPrecioAcab1.TabIndex = 10;
+            this.lblPrecioAcab1.Text = "$200.000";
+            // 
+            // lblAcab1
+            // 
+            this.lblAcab1.Location = new System.Drawing.Point(39, 154);
+            this.lblAcab1.Name = "lblAcab1";
+            this.lblAcab1.Size = new System.Drawing.Size(73, 28);
+            this.lblAcab1.TabIndex = 9;
+            this.lblAcab1.Text = "Buso A.C.A.B Talla L";
+            // 
+            // pbTenis1
+            // 
+            this.pbTenis1.Image = global::PROCESO_CRUD.Properties.Resources.zeus_1;
+            this.pbTenis1.Location = new System.Drawing.Point(412, 263);
+            this.pbTenis1.Name = "pbTenis1";
+            this.pbTenis1.Size = new System.Drawing.Size(120, 132);
+            this.pbTenis1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbTenis1.TabIndex = 7;
+            this.pbTenis1.TabStop = false;
+            // 
+            // pbTenis2
+            // 
+            this.pbTenis2.Image = global::PROCESO_CRUD.Properties.Resources.zeus;
+            this.pbTenis2.Location = new System.Drawing.Point(412, 19);
+            this.pbTenis2.Name = "pbTenis2";
+            this.pbTenis2.Size = new System.Drawing.Size(120, 132);
+            this.pbTenis2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbTenis2.TabIndex = 6;
+            this.pbTenis2.TabStop = false;
+            // 
+            // pbCamisa2
+            // 
+            this.pbCamisa2.Image = global::PROCESO_CRUD.Properties.Resources.pirlo_1;
+            this.pbCamisa2.Location = new System.Drawing.Point(226, 263);
+            this.pbCamisa2.Name = "pbCamisa2";
+            this.pbCamisa2.Size = new System.Drawing.Size(120, 132);
+            this.pbCamisa2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbCamisa2.TabIndex = 5;
+            this.pbCamisa2.TabStop = false;
+            // 
+            // pbCamisa1
+            // 
+            this.pbCamisa1.Image = global::PROCESO_CRUD.Properties.Resources.pirlo1;
+            this.pbCamisa1.Location = new System.Drawing.Point(42, 263);
+            this.pbCamisa1.Name = "pbCamisa1";
+            this.pbCamisa1.Size = new System.Drawing.Size(120, 132);
+            this.pbCamisa1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbCamisa1.TabIndex = 4;
+            this.pbCamisa1.TabStop = false;
+            // 
+            // pbBuso1
+            // 
+            this.pbBuso1.Image = global::PROCESO_CRUD.Properties.Resources.acab;
+            this.pbBuso1.Location = new System.Drawing.Point(42, 19);
+            this.pbBuso1.Name = "pbBuso1";
+            this.pbBuso1.Size = new System.Drawing.Size(120, 132);
+            this.pbBuso1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbBuso1.TabIndex = 0;
+            this.pbBuso1.TabStop = false;
+            // 
+            // pbBuso2
+            // 
+            this.pbBuso2.Image = global::PROCESO_CRUD.Properties.Resources.acab__2_;
+            this.pbBuso2.Location = new System.Drawing.Point(226, 19);
+            this.pbBuso2.Name = "pbBuso2";
+            this.pbBuso2.Size = new System.Drawing.Size(120, 132);
+            this.pbBuso2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbBuso2.TabIndex = 2;
+            this.pbBuso2.TabStop = false;
+            // 
+            // frmGaleria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 480);
+            this.Controls.Add(this.gbRopa);
+            this.Name = "frmGaleria";
+            this.Text = "frmGaleria";
+            this.gbRopa.ResumeLayout(false);
+            this.gbRopa.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTenis1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTenis2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCamisa2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCamisa1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBuso1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBuso2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbBuso1;
+        private System.Windows.Forms.PictureBox pbBuso2;
+        private System.Windows.Forms.GroupBox gbRopa;
+        private System.Windows.Forms.PictureBox pbCamisa2;
+        private System.Windows.Forms.PictureBox pbCamisa1;
+        private System.Windows.Forms.PictureBox pbTenis1;
+        private System.Windows.Forms.PictureBox pbTenis2;
+        private System.Windows.Forms.Label lblAcab1;
+        private System.Windows.Forms.Button btnBuso1;
+        private System.Windows.Forms.Label lblPrecioTenis2;
+        private System.Windows.Forms.Label lblTenis2;
+        private System.Windows.Forms.Label lblPrecioTenis1;
+        private System.Windows.Forms.Label lblTenis1;
+        private System.Windows.Forms.Label lblPrecioCamisa2;
+        private System.Windows.Forms.Label lblcamisa2;
+        private System.Windows.Forms.Label lblPrecioCamisa1;
+        private System.Windows.Forms.Label lblcamisa1;
+        private System.Windows.Forms.Label lblPrecioAcab2;
+        private System.Windows.Forms.Label lblAcab2;
+        private System.Windows.Forms.Label lblPrecioAcab1;
+        private System.Windows.Forms.Button btnTenis2;
+        private System.Windows.Forms.Button btnTenis1;
+        private System.Windows.Forms.Button btnCamisa2;
+        private System.Windows.Forms.Button btnCamisa1;
+        private System.Windows.Forms.Button btnBuso2;
+    }
+}
